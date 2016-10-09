991a06a27eeee08fec328be368bfe4dc9810267c
rabinzon <ildarrazin@gmail.com>
2016-10-09 17:56:11 +0300

    feat: add test for 1.3

--- test/sicp/ch1_test.clj
+++ test/sicp/ch1_test.clj
@@ -1,10 +1,12 @@
 (ns sicp.ch1-test
-  (:require [clojure.test :refer :all]
-            [sicp.ch1 :refer :all]))
+	(:require [clojure.test :refer :all]
+		[sicp.ch1 :refer :all]))
 
 (defn assert-equal [x y]
-  (is (= x y)))
+	(is (= x y)))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (assert-equal (foo) "Hello, World!")))
+(deftest ch1-1-3-test
+	(testing "biggest-sqr-sum is failed"
+		(assert-equal (biggest-sqr-sum 2 2 3) 13)
+		(assert-equal (biggest-sqr-sum 1 2 0) 5)
+		(assert-equal (biggest-sqr-sum 2 0 1) 5)))
