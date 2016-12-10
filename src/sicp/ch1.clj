34d59518e0664ffcd54c74c8dd967102e1cbe65f
rabinzon <ildarrazin@gmail.com>
2016-12-10 08:49:59 +0300

    feat: add ex 1.8

--- src/sicp/ch1.clj
+++ src/sicp/ch1.clj
@@ -11,18 +11,18 @@
 	(+ (sqr x) (sqr y)))
 
 (defn biggest-sqr-sum [x, y, z]
-	(if (biggest x y) 
-		(if (biggest y z) 
-			(sqr-sum x y) 
-			(sqr-sum x z)) 
-		(if (biggest x z) 
-			(sqr-sum x y) 
+	(if (biggest x y)
+		(if (biggest y z)
+			(sqr-sum x y)
+			(sqr-sum x z))
+		(if (biggest x z)
+			(sqr-sum x y)
 			(sqr-sum y z))))
 ;; end 1.3
 
 ;; ex 1.11
 (defn f-rec [n]
-	(if (< n 3) n 
+	(if (< n 3) n
 		(+	(f-rec (- n 1))
 			(f-rec (- n 2))
 			(f-rec (- n 3)))))
@@ -30,9 +30,9 @@
 
 ;; ex 1.16
 
-(defn fast-expt-iter [x, degree, acc] 
+(defn fast-expt-iter [x, degree, acc]
 	(if (= degree 0) acc
-		(if (even? degree) 
+		(if (even? degree)
 			(fast-expt-iter x (- degree 2) (* acc (sqr x)))
 			(fast-expt-iter x (dec degree) (* acc x)))))
 
@@ -40,3 +40,46 @@
 	(fast-expt-iter x degree 1))
 
 ;; end 1.16
+
+;; ex 1.7
+(defn abs [x]
+	(if (> x 0) x (* -1 x)))
+
+(defn sqrt [x]
+	(defn good-enough? [guess, x]
+		(< (abs (- (sqr guess) x)) 0.001))
+
+	(defn average [x, y]
+		(/ (+ x y) 2))
+
+	(defn improve [guess, x]
+		(average guess (/ x guess)))
+
+	(defn sqrt-iter [guess, x]
+		(if (good-enough? guess x) guess
+			(sqrt-iter (improve guess x) x)))
+	(sqrt-iter 1.0 x))
+
+
+;; end 1.7
+
+;; 1.8
+(defn cube [x]
+	(* x x x))
+
+(defn	cube-root [x]
+	(defn good-enough? [guess, x]
+		(< (abs (- (cube guess) x)) 0.001))
+
+	(defn average [x, y]
+		(/ (+ x y) 2))
+
+	(defn improve [guess, x]
+		(average guess
+				 (/ (+ (/ x (sqr guess)) (* 2 guess)) 3)))
+
+	(defn cube-iter [guess, x]
+		(if (good-enough? guess x) guess
+			(cube-iter (improve guess x) x)))
+	(cube-iter 1.0 x))
+;; end 1.8
